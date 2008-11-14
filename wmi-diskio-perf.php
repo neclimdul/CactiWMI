e78c75eb7cf7fbe59acef1d4c07b1649c622acd4
Ross Fawcett <claymen@parkingdenied.com>
2008-11-14 07:34:57 +0000

    Updated to suit some changes in the includes.php

--- wmi-diskio-perf.php
+++ wmi-diskio-perf.php
@@ -8,11 +8,11 @@
 
 include('wmi-includes.php');
 
-// Query to run
+// Query to run - could be worth splitting this down into cleaner variables for ease of reading and also generating help.
 $query = "wmic -U ".$argv[2]."%".$argv[3]." //".$argv[1]." \"select DiskReadBytesPersec,DiskReadsPersec,DiskWriteBytesPersec,DiskWritesPersec,Frequency_PerfTime,Timestamp_PerfTime from Win32_PerfRawData_PerfDisk_LogicalDisk where Name='".$argv[4]."'\"";
 
 
-// Filename for tmp storage
+// Filename for tmp storage - consider saving this in the includes file maybe?
 $filename = '/tmp/wmi_new_'.$argv[1].'_'.$argv[4];
 
 
@@ -21,10 +21,15 @@ $output = null;
 
 // Run the query
 exec($query,$query_output);
+
+// Pull the data we need out of it
 $data1 = explode("|",$query_output[2]);
 $names = explode("|",$query_output[1]);
 
-// Check to see if the file exists, if it does open it
+/*
+Check to see if the file exists, if it does open it.
+If the file does not exist use the current data which will result in 0's on the output to make things clean.
+*/
 if (file_exists($filename)) {
 $data2 = unserialize(file_get_contents($filename));
 } else {
@@ -33,18 +38,21 @@ $data2 = $data1;
 
 $arr = array();
 
+
+// Use the names pulled from wmic rather than hand code. Could make this dynamic at some stage.
 for($i=0;$i<4;$i++) {
 $arr[$names[$i]] = (int) counter_counter($data1[$i],$data2[$i],$data1[6],$data2[6],$data2[4]);
 };
 
+// Output using the function to make things easy.
 $output = process_output($arr);
 
-// Write new data to the tmp file and then close it
+// Serialize and then write the data to the tmp file and then close it.
 $fp = fopen($filename,'w');
 fwrite($fp, serialize($data1));
 fclose($fp);
 
+// Display the output.
 echo $output;
-
 
 ?>
