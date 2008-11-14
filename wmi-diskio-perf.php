c603dfc89d8d1eb87356a6d4c94d36f9cf948cbf
Ross Fawcett <claymen@parkingdenied.com>
2008-11-14 06:13:17 +0000

    Rewrote to use the output function

--- wmi-diskio-perf.php
+++ wmi-diskio-perf.php
@@ -6,12 +6,14 @@
  * @copyright 2008
  */
 
+include('wmi-includes.php');
+
 // Query to run
 $query = "wmic -U ".$argv[2]."%".$argv[3]." //".$argv[1]." \"select DiskReadBytesPersec,DiskReadsPersec,DiskWriteBytesPersec,DiskWritesPersec,Frequency_PerfTime,Timestamp_PerfTime from Win32_PerfRawData_PerfDisk_LogicalDisk where Name='".$argv[4]."'\"";
 
 
 // Filename for tmp storage
-$filename = '/tmp/wmi_'.$argv[1].'_'.$argv[4];
+$filename = '/tmp/wmi_new_'.$argv[1].'_'.$argv[4];
 
 
 // Preset output variable
@@ -20,26 +22,34 @@ $output = null;
 // Run the query
 exec($query,$query_output);
 $data1 = explode("|",$query_output[2]);
-unset($query_output);
-
+$names = explode("|",$query_output[1]);
 
 // Check to see if the file exists, if it does open it
 if (file_exists($filename)) {
 $data2 = unserialize(file_get_contents($filename));
 
-$output =
+$arr = array();
+for($i=0;$i<4;$i++) {
+$arr[$names[$i]] = (int) counter_counter($data1[$i],$data2[$i],$data1[6],$data2[6],$data2[4]);
+};
+$output = process_output($arr);
+
+
+/*$output =
 "DiskReadBPS:" . (int) counter_counter($data1[0],$data2[0],$data1[6],$data2[6],$data2[4]) . " " .
 "DiskReadsPS:" . (int) counter_counter($data1[1],$data2[1],$data1[6],$data2[6],$data2[4]) . " " .
 "DiskWriteBPS:". (int) counter_counter($data1[2],$data2[2],$data1[6],$data2[6],$data2[4]) . " " .
-"DiskWritesPS:". (int) counter_counter($data1[3],$data2[3],$data1[6],$data2[6],$data2[4]);
+"DiskWritesPS:". (int) counter_counter($data1[3],$data2[3],$data1[6],$data2[6],$data2[4]);*/
 } else {
-$output =
-"DiskReadBPS:" . 0 . " " .
-"DiskReadsPS:" . 0 . " " .
-"DiskWriteBPS:". 0 . " " .
-"DiskWritesPS:". 0;
 
+$arr = array();
+for($i=0;$i<4;$i++) {
+$arr[$names[$i]] = 0;
+};
+$output = process_output($arr);
 }
+
+
 
 
 // Write new data to the tmp file and then close it
