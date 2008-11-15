5c551c2fc2e6d81acc89e81d5da38de11a02d521
Ross Fawcett <claymen@parkingdenied.com>
2008-11-15 04:33:25 +0000

    Code formatting.

--- wmi-includes.php
+++ wmi-includes.php
@@ -2,7 +2,7 @@
 
 /*
  +-------------------------------------------------------------------------+
- | Copyright (C) 2008 Ross Fawcett	                                       |
+ | Copyright (C) 2008 Ross Fawcett                                         |
  |                                                                         |
  | This program is free software; you can redistribute it and/or           |
  | modify it under the terms of the GNU General Public License             |
