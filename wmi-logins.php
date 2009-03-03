84770988ffe83ee5c42151c8b372c949b9ad9df1
Ross Fawcett <claymen@parkingdenied.com>
2009-03-03 09:47:16 +0000

    Code cleanup and comments for Issue #2
    Will do some further cleanup at a later stage.

--- wmi-logins.php
+++ wmi-logins.php
@@ -16,10 +16,27 @@
 */
 
 
+/*
+
+This file defines the login/password pairs and the reference used by wmi.php
+to access them. This is done to prevent the credentials being logged in the
+Cacti log files and allow some separation.
+
+To add another login credential add it below like follows.
+
+$logins['reference'] = array('Domain/Username','Password');
+
+Reference is the single word name for the username/password pair. And the two
+fields in the array are the actual username (with domain if required) and the
+password for that user.
+
+One last note, by default the templates don't offer a different username per
+host, you simply need to change this in the template.
+
+*/
 
 
 $logins = array();
-$logins['cacti'] = array('Domain/Username','Password');
-$logins['testing'] = array('Domain/Username','Password');
+$logins['credential'] = array('Domain/Username','Password');
 
 ?>
