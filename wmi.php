0eda6a6a5e529030176b3ef500c36555bda747e9
Ross Fawcett <claymen@parkingdenied.com>
2009-11-24 05:46:35 +0000

    Updated version as this is the SVN version not 0.0.6

--- wmi.php
+++ wmi.php
@@ -21,7 +21,7 @@ $output = null; // by default the output is null
 $inc = null; // by default needs to be null
 $sep = " "; // character to use between results
 $dbug_levels = array(0,1,2); // valid debug levels
-$version = '0.0.6'; // version
+$version = '0.0.7-SVN'; // version
 $namespace = escapeshellarg('root\CIMV2'); // default namespace
 $columns = '*'; // default to select all columns
 
